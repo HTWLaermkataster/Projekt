b4dd2724a2418b6af13529dea212f291fed95a5d
Jennie123 <s0536831@htw-berlin.de>
2014-11-16 15:29:46 +0100

    Phase Konstruktion

---
+++ 3 Phase Konstruktion/Projekt_Laerm/Projekt_Laerm/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Projekt_Laerm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(427, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(250, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Verbindung zum SQL Server";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(427, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(250, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Daten auslesen";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(427, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(250, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Daten speichern";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(427, 99);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(250, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Beenden";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 436);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(21, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 462);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ 3 Phase Konstruktion/Projekt_Laerm/Projekt_Laerm/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Timers;
+using System.Threading;
+using System.Data.OleDb;
+
+namespace Projekt_Laerm
+{
+    public partial class Form1 : Form
+    {
+        const int AuflX = 400;
+        const int AuflY = 400;
+
+        int[] Data = new int[AuflX * AuflY];
+        int[] DataHG = new int[AuflX * AuflY];
+
+        int xpos, ypos;
+        System.Timers.Timer aTimer;
+
+        public void SetPixel(int x, int y, int Farbe)
+        {
+            if (x < AuflX && x > 0 && y < AuflY && y > 0)
+                Data[x + y * AuflX] = Farbe;
+        }
+
+        public void SetKreis(int xpos, int ypos, int rad, int Farbe)
+        {
+            int x, y;
+
+            for (x = xpos - rad * 2; x < xpos + rad * 2; x++)
+                for (y = ypos - rad * 2; y < ypos + rad * 2; y++)
+                    if (Math.Sqrt((x - xpos) * (x - xpos) + (y - ypos) * (y - ypos)) < rad)
+                        SetPixel(x, y, Farbe);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.pictureBox1.MouseMove += new MouseEventHandler(pictureBox1_MouseMove);
+            this.pictureBox1.MouseClick += new MouseEventHandler(pictureBox1_MouseClick);
+            this.Size = new System.Drawing.Size(AuflX + 300, AuflY + 100);
+            this.pictureBox1.Size = new System.Drawing.Size(AuflX, AuflY);
+
+            aTimer = new System.Timers.Timer();
+            aTimer.Elapsed += new ElapsedEventHandler(BildSetzen);
+            aTimer.Interval = 20;                                           // alle 20 ms die Mal-Prozedur aufrufen -> entspricht 50 Hertz
+            aTimer.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Datenbankverbindung();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+        void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            xpos = e.X;
+            ypos = e.Y;
+
+            label1.Text = "XPosition: " + xpos.ToString() + ", YPosition: " + ypos.ToString();
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        { //if (e.Button == MouseButtons.Left) Werte[Jahr] = Wert;
+            SetKreis(xpos, ypos, 15, 0xff6622);
+//            aTimer.Enabled = false;             // Timer abschalten und 30 ms warten. Dann Hintergrund neu zeichnen und Timer wieder anschalten
+//            Thread.Sleep(30);
+
+            //Hintergrund();
+//            aTimer.Enabled = true;
+
+        }
+        public void BildSetzen(object source, ElapsedEventArgs e)
+        {
+//            Hintergrund_copy();
+
+            // Bild generieren
+
+            unsafe
+            {
+                fixed (int* pointer = Data)
+                {
+                    System.IntPtr ptr = new System.IntPtr(pointer);
+                    System.Drawing.Bitmap flag = new System.Drawing.Bitmap(AuflX, AuflY, AuflX * 4, PixelFormat.Format32bppArgb, ptr);
+                    pictureBox1.Image = flag;
+                }
+            }
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Datenbankverbindung();
+            SetKreis(xpos, ypos, 15, 0xff6622);
+        }
+
+        private void flowLayoutPanel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        public void Datenbankverbindung()
+        {
+            string strAccessConn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=..\\..\\..\\DB_Laerm.accdb";
+            string strAccessSelect = "SELECT * FROM Daten";
+            DataSet myDataSet = new DataSet();
+            OleDbConnection myAccessConn = null;
+            try
+            {
+                myAccessConn = new OleDbConnection(strAccessConn);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: Failed to create a database connection. \n{0}", ex.Message);
+                return;
+            }
+
+            try
+            {
+
+                OleDbCommand myAccessCommand = new OleDbCommand(strAccessSelect, myAccessConn);
+                OleDbDataAdapter myDataAdapter = new OleDbDataAdapter(myAccessCommand);
+
+                myAccessConn.Open();
+                myDataAdapter.Fill(myDataSet, "Daten");
+
+            }
+            catch (Exception ex)
+            {
+                //MessageBox();
+                return;
+            }
+            finally
+            {
+                myAccessConn.Close();
+            }
+        }
+    }
+}
