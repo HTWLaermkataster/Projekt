0635832b37ad6e8dfc02e94f018f482e48d50d02
Jennie123 <s0536831@htw-berlin.de>
2015-01-06 16:01:33 +0100

    4 Phase Transition

---
+++ 4 Phase Transition/Visual_Jennie/pManagement_überbearbeitet_version/pManagement/Messwert.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pManagement
+{
+    class Messwert
+    {
+        public string Ort { get; set; }
+        public int Wert { get; set; } 
+        public int Xwert { get; set; }
+        public int Ywert { get; set; }
+        public Messwert ( string Ort, int Wert, int Xwert, int Ywert)
+        {
+            this.Ort = Ort;
+            this.Wert = Wert;
+            this.Ywert = Ywert;
+            this.Xwert = Xwert;
+        }
+        public Messwert(){}
+    }
+}
